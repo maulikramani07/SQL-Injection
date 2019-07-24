9b5e9192f45e12473f4d324abcab4ef22af457ee
Maulik Ramani <17it093@charusat.edu.in>
2019-07-24 09:29:11 +0530

    Initial Commit

---
+++ search.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Search bar</title>
+</head>
+<body>
+	<form action="print.php" method="POST">
+	<label>Search : </label>
+	<input type="text" name="search" placeholder="Search Here">
+	<input type="submit" name="submit" value="Submit">
+	</form>
+	<br>
+	<br>
+
+	
+
+</body>
+</html>
